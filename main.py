81684702e6ca284a6f5e865b25a31347008c04f9
JohnDavid <jhndagon@gmail.com>
2021-03-19 09:43:18 -0500

    update changes

--- main.py
+++ main.py
@@ -1,11 +1,69 @@
-from fastapi import FastAPI
+from typing import Dict, List
+import requests
+from fastapi import FastAPI, HTTPException, status
+from fastapi.datastructures import UploadFile
+from fastapi.params import File
 
-from src.schema.predict import PredictRequest, PredictResponse
+import pandas as pd
+
+from src.schema.predict import PredictRequest, PredictResponse, Predict
+
+from src.processor.similarity import get_cosine
 
 app = FastAPI()
 
 
+@app.post(
+    "/predict/file"
+)
+async def predict_file(
+    file: UploadFile = File(...)
+):
+    extention = file.filename.split('.')[1]
+    file_data = file.file.read()
+    if extention == "csv":
+        file_read = pd.read_csv(file_data)
+    elif extention == 'xlsx' or extention == 'xls':
+        file_read = pd.read_excel(file_data)
+    else:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail='The file does not have an appropriate extension')
 
+    columns = file_read.columns
+    weight = []
+    volume = []
+    for column in columns:
+        if get_cosine(column, "Weight") >= 0.75:
+            weight = file_read[column].to_list()
+        elif get_cosine(column, "Cubic Feet") >= 0.75:
+            volume = file_read[column].to_list()
+    
+    if not weight or not volume:
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail='The fields do not have the same quantity of rows')
+    
+    data =  [
+        Predict(
+            **requests.post("https://8hw1id.deta.dev/predict", json={
+                "weight": weig,
+                "volume": vol
+            }).json()
+        )
+        for vol, weig in zip(weight, volume)
+    ]
+    data_ = list(map(lambda x: x.dict(), data))
+    data_df = pd.DataFrame(data_)
+    
+    length = abs(file_read['Length'] - data_df['length'])  /  file_read['Length']
+    width = abs(file_read['Width'] - data_df['width'])  /  file_read['Width']
+    height = abs(file_read['Height'] - data_df['height'])  /  file_read['Height']
+        
+    data_df["mape"] = 100 *  (length + width + height) / 3     
+    
+    
+    mape = data_df["mape"].sum() / len(data_df["mape"])
+    error_pond = sum(list(map(lambda x: x.error, data))) / len(data)
+    
+    return PredictResponse(predict=data, volume_error=error_pond, mape=mape)
+                
 
 
 @app.post(
@@ -15,9 +73,24 @@ app = FastAPI()
 async def predict(
     body: PredictRequest
 ):
-    return PredictResponse(
-        length=26,
-        width=27,
-        height=29,
-        n_boxes=3
-    )
+    weights = body.weight.split(',')
+    volumes = body.volume.split(',')
+    
+    weights = map(lambda weight: float(weight), weights)
+    volumes = map(lambda volume: float(volume), volumes)
+        
+    data =  [
+        Predict(
+            **requests.post("https://8hw1id.deta.dev/predict", json={
+                "weight": weight,
+                "volume": volume
+            }).json()
+        )
+        for weight, volume in zip(weights, volumes)
+    ]
+    
+    error_pond = sum(list(map(lambda x: x.error, data))) / len(data)
+    
+    return PredictResponse(predict=data, volume_error=error_pond)
+
+
--- src/schema/predict.py
+++ src/schema/predict.py
@@ -1,13 +1,22 @@
+from typing import List
 from pydantic import BaseModel
 
 
 class PredictRequest(BaseModel):
-    weight: float
-    volume: float
+    weight: str
+    volume: str
     
 
-class PredictResponse(BaseModel):
+class Predict(BaseModel):
     length: float
     height: float
     width: float
     n_boxes: int
+    error: float
+    mape: float = 0
+    
+
+class PredictResponse(BaseModel):
+    predict: List[Predict]
+    volume_error: float
+    mape: float = 0
